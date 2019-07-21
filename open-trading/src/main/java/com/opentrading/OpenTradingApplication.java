5b0e64fc77d6d82c2cd90ad3a9c7188778b304bd
Shashank Dixit <shashanksdixit@gmail.com>
2019-07-21 19:21:42 +0530

    Created spring boot project

---
+++ open-trading/src/main/java/com/opentrading/OpenTradingApplication.java
@@ -0,0 +1,13 @@
+package com.opentrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTradingApplication.class, args);
+	}
+
+}
